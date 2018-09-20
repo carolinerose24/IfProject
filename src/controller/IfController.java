7e0b19fc7c5bd9db23f6502e5070c19eef5c1137
Caroline Jarman <carolinerosejarman@gmail.com>
2018-09-20 07:44:53 -0600

    If Project first commit, added some popups

---
+++ src/controller/IfController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class IfController
+{
+	public void start ()
+	{
+		String MovieQuestion = JOptionPane.showInputDialog(null, "What is your favorite movie genre?\nComedy, Action, or Romance?");
+		
+		String AgeMinor = "17"; 
+		
+		
+		
+		
+	}
+}
